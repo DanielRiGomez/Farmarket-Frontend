a3016a6b15abd2ee1f9bf436fe88ffab7db4d9b3
DanielRiGomez <daniel.rgomez@hotmail.com>
2022-07-19 15:39:28 -0500

    GUI modulos de usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { UserPerfilComponent } from './user-perfil/user-perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MainMenuComponent,
+    UserPerfilComponent
   ],
   imports: [
     BrowserModule,
