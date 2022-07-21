6d43fbcb2db91d1f39a896deb9a35351cec5a36f
DanielRiGomez <daniel.rgomez@hotmail.com>
2022-07-20 19:53:07 -0500

    Configuracion Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {MainMenuComponent} from "./main-menu/main-menu.component";
+import {UserPerfilComponent} from "./user-perfil/user-perfil.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: LoginComponent},
+  {path:'registro', component: RegisterComponent},
+  {path:'inicio', component: MainMenuComponent},
+  {path:'perfil', component: UserPerfilComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/user-perfil/user-perfil.component.ts
+++ src/app/user-perfil/user-perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-user-perfil',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserPerfilComponent implements OnInit {
 
-  constructor() { }
+  btnInit(){
+    this.router.navigateByUrl('/inicio')
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
